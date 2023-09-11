5b88adb709efce23dbbe50c60a12f87c2af084fd
Franak007 <franck.pertosa@gmail.com>
2023-09-11 15:44:33 +0200

    component Pokemon-page avec routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    PokemonPageComponent,
   ],
   imports: [
     BrowserModule,
